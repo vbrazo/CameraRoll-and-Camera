a0f48bf1a88c48a1bcb0a58454209754361c7308
Vitor Oliveira <vbrazo@gmail.com>
2016-02-26 19:07:40 +0200

    Creates a Camera Controller to store all the main camera functions

--- Camera/Camera.swift
+++ Camera/Camera.swift
@@ -7,30 +7,17 @@
 //
 
 import UIKit
-import MobileCoreServices
-import AVFoundation
 
-class Camera: UIViewController {
+class Camera : UIViewController {
     
-    let global = Global()
-    var photo : UIImageView!
+    @IBOutlet weak var cameraOptionsView: UIView!
     
-    let devices = AVCaptureDevice.devices()
-    var captureDevice : AVCaptureDevice?
-    let captureSession = AVCaptureSession()
-    var previewLayer : AVCaptureVideoPreviewLayer?
-    let stillImageOutput = AVCaptureStillImageOutput()
-    var cameraPosition = AVCaptureDevicePosition.Back
-    
+    let camera = CameraController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        stillImageOutput.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
-        captureSession.sessionPreset = AVCaptureSessionPresetHigh
-        
-        if device(AVCaptureDevicePosition.Back) {
-            startCamera()
-        }
+        camera.startCamera(self.view)
+        self.view.bringSubviewToFront(cameraOptionsView)
     }
     
     override func didReceiveMemoryWarning() {
@@ -38,101 +25,20 @@ class Camera: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func startCamera(){
-        
-        if captureSession.canAddOutput(stillImageOutput) {
-            captureSession.addOutput(stillImageOutput)
-        }
-        
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
-        previewLayer?.frame = CGRectMake(0, 0, self.global.screenWidth, self.global.screenHeight-100)
-        
-        self.view.layer.addSublayer(previewLayer!)
-        
-        captureSession.startRunning()
-
-    }
-    
-    func device(type: AVCaptureDevicePosition) -> Bool {
-        
-        for device in devices {
-            if (device.hasMediaType(AVMediaTypeVideo)) {
-                if(device.position == type) {
-                    captureDevice = device as? AVCaptureDevice
-                    if captureDevice != nil {
-                        do {
-                            try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
-                        } catch {
-                            print("Error")
-                        }
-                    }
-                }
-            }
-        }
-        
-        return captureDevice != nil
-        
-    }
-    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
-        let focusX = touches.first!.locationInView(self.view).x / self.global.screenWidth
-        let focusY = touches.first!.locationInView(self.view).y / self.global.screenHeight
-
-        if cameraPosition == .Back {
-            if let device = captureDevice {
-                do {
-                    try device.lockForConfiguration()
-                    device.focusPointOfInterest = CGPointMake(focusX, focusY)
-                    device.focusMode = AVCaptureFocusMode.AutoFocus
-                    device.exposurePointOfInterest = CGPointMake(focusX, focusY)
-                    device.exposureMode = AVCaptureExposureMode.ContinuousAutoExposure
-                    device.unlockForConfiguration()
-                } catch let error as NSError {
-                    print(error.code)
-                }
-            }
-        }
-        
+        camera.touchesBegan(touches, withEvent: event, view: self.view)
     }
     
     @IBAction func btnTakePictureClick(sender: AnyObject) {
-        dispatch_async(dispatch_get_main_queue(), {
-            if let videoConnection = self.stillImageOutput.connectionWithMediaType(AVMediaTypeVideo){
-                self.stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {
-                    (sampleBuffer, error) in
-                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
-                    let dataProvider = CGDataProviderCreateWithCFData(imageData)
-                    let cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, CGColorRenderingIntent.RenderingIntentDefault )
-                    let image = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.Right)
-                    
-                    let imageView = UIImageView(image: image)
-                    imageView.frame = CGRect(x:0, y:0, width:self.view.frame.width/2, height:self.view.frame.height/2)
-                    
-                    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-                    
-                    self.photo = imageView
-                    self.captureSession.stopRunning()
-                    
-                    let vc = self.storyboard?.instantiateViewControllerWithIdentifier("PreviewID") as! Preview
-                    vc.newMediaImage = imageView
-                    self.presentViewController(vc, animated: false, completion: nil)
-                    
-                })
-            }
+        camera.takePicture(self.view, imageGenerated: { (image, error) -> Void in
+            let vc = self.storyboard?.instantiateViewControllerWithIdentifier("PreviewID") as! Preview
+            vc.newMediaImage = image
+            self.presentViewController(vc, animated: false, completion: nil)
         })
-        
     }
 
     @IBAction func changeCameraPosition(sender: AnyObject) {
-        
-        let currentInput = captureSession.inputs[0] as! AVCaptureInput
-        captureSession.removeInput(currentInput)
-        
-        cameraPosition = cameraPosition == .Back ? .Front : .Back
-        self.device(cameraPosition)
-       
+        camera.changeCameraPosition()
     }
         
     @IBAction func btnLibraryClick(sender: AnyObject) {
---
+++ Camera/CameraController.swift
@@ -0,0 +1,119 @@
+//
+//  CameraPod.swift
+//  CameraPod
+//
+//  Created by Vitor Oliveira on 2/26/16.
+//
+//
+
+import UIKit
+import MobileCoreServices
+import AVFoundation
+
+public class CameraController {
+    
+    private let global = Global()
+    
+    private let devices = AVCaptureDevice.devices()
+    private var captureDevice : AVCaptureDevice?
+    private let captureSession = AVCaptureSession()
+    private var previewLayer : AVCaptureVideoPreviewLayer?
+    private let stillImageOutput = AVCaptureStillImageOutput()
+    private var cameraPosition = AVCaptureDevicePosition.Back
+    
+    public func startCamera(view: UIView){
+        
+        stillImageOutput.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+        
+        if device(AVCaptureDevicePosition.Back) {
+            if captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addOutput(stillImageOutput)
+            }
+        
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            previewLayer?.frame = CGRectMake(0, 0, self.global.screenWidth, self.global.screenHeight-45)
+        
+            view.layer.addSublayer(previewLayer!)
+        
+            captureSession.startRunning()
+        }
+        
+    }
+    
+    public func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?, view: UIView) {
+        
+        let focusX = touches.first!.locationInView(view).x / self.global.screenWidth
+        let focusY = touches.first!.locationInView(view).y / self.global.screenHeight
+        
+        if cameraPosition == .Back {
+            if let device = captureDevice {
+                do {
+                    try device.lockForConfiguration()
+                    device.focusPointOfInterest = CGPointMake(focusX, focusY)
+                    device.focusMode = AVCaptureFocusMode.AutoFocus
+                    device.exposurePointOfInterest = CGPointMake(focusX, focusY)
+                    device.exposureMode = AVCaptureExposureMode.ContinuousAutoExposure
+                    device.unlockForConfiguration()
+                } catch let error as NSError {
+                    print(error.code)
+                }
+            }
+        }
+        
+    }
+    
+    public func takePicture(view: UIView, imageGenerated: (UIImageView?, NSError?) -> Void) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if let videoConnection = self.stillImageOutput.connectionWithMediaType(AVMediaTypeVideo){
+                self.stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {
+                    (sampleBuffer, error) in
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    let dataProvider = CGDataProviderCreateWithCFData(imageData)
+                    let cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, CGColorRenderingIntent.RenderingIntentDefault )
+                    let image = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.Right)
+                    
+                    let imageView = UIImageView(image: image)
+                    imageView.frame = CGRect(x:0, y:0, width:view.frame.width/2, height:view.frame.height/2)
+                    
+                    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                    
+                    self.captureSession.stopRunning()
+                    
+                    imageGenerated(imageView, nil)
+                })
+            }
+        })
+    }
+    
+    public func changeCameraPosition() {
+        let currentInput = captureSession.inputs[0] as! AVCaptureInput
+        captureSession.removeInput(currentInput)
+        
+        cameraPosition = cameraPosition == .Back ? .Front : .Back
+        self.device(cameraPosition)
+    }
+    
+    private func device(type: AVCaptureDevicePosition) -> Bool {
+        
+        for device in devices {
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                if(device.position == type) {
+                    captureDevice = device as? AVCaptureDevice
+                    if captureDevice != nil {
+                        do {
+                            try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+                        } catch {
+                            print("Error")
+                        }
+                    }
+                }
+            }
+        }
+        
+        return captureDevice != nil
+        
+    }
+    
+}
